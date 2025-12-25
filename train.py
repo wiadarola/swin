079084b1a6bf59730632f4d556c012a00bbdbc0a
William Iadarola <iadarola.william@gmail.com>
2025-12-25 04:20:43 +0000

    Compile

--- train.py
+++ train.py
@@ -10,14 +10,16 @@ from torch import nn
 from torch.optim.lr_scheduler import CosineAnnealingLR, LinearLR, SequentialLR
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-from torchvision.datasets import CIFAR10
+from torchvision.datasets import CIFAR100
 from tqdm.auto import tqdm
 
-from src.model import SwinTransformer
+from model import SwinTransformer
 
 
 @hydra.main(config_path="config", config_name="config", version_base=None)
 def main(cfg: DictConfig):
+    torch.set_float32_matmul_precision("high")
+
     log_dir = HydraConfig.get().run.dir
     writer = SummaryWriter(log_dir)
 
@@ -26,23 +28,26 @@ def main(cfg: DictConfig):
 
     metrics = torchmetrics.MetricCollection(
         {
-            "f1": torchmetrics.F1Score(task="multiclass", num_classes=10),
-            "accuracy": torchmetrics.Accuracy(task="multiclass", num_classes=10),
+            "f1": torchmetrics.F1Score(task="multiclass", num_classes=100),
+            "accuracy": torchmetrics.Accuracy(task="multiclass", num_classes=100),
         }
     ).to(device)
 
     train_loss = torchmetrics.MeanMetric().to(device)
     valid_loss = torchmetrics.MeanMetric().to(device)
 
-    train_metrics = metrics.clone(postfix="train/")
-    valid_metrics = metrics.clone(postfix="valid/")
+    train_metrics = metrics.clone(postfix="/train")
+    valid_metrics = metrics.clone(postfix="/valid")
 
     logging.info(f"Computing metrics: {[metric for metric in metrics]}")
 
+    epochs_without_improvement = 0
     best_valid_loss = torch.inf
 
-    model = SwinTransformer(**cfg.model, n_classes=10).to(device)
-    criterion = nn.CrossEntropyLoss()
+    model = SwinTransformer(**cfg.model, n_classes=100).to(device)
+    model.compile(fullgraph=True)
+
+    criterion = nn.CrossEntropyLoss(**cfg.criterion)
     optimizer = torch.optim.AdamW(model.parameters(), **cfg.optimizer)
 
     logging.info(f"Using model: {HydraConfig.get().runtime.choices.get('model')}")
@@ -55,9 +60,9 @@ def main(cfg: DictConfig):
     ]
     lr_scheduler = SequentialLR(optimizer, schedulers, [warmup_steps])
 
-    toTensor = T.Compose((T.ToImage(), T.ToDtype(torch.float, scale=True)))
-    train_set = CIFAR10("data/", transform=toTensor, download=True)
-    valid_set = CIFAR10("data/", train=False, transform=toTensor, download=True)
+    toTensor = T.Compose((T.ToImage(), T.ToDtype(torch.float, scale=True), T.Resize((256, 256))))
+    train_set = CIFAR100("data/", train=True, transform=toTensor, download=True)
+    valid_set = CIFAR100("data/", train=False, transform=toTensor, download=True)
     train_loader = DataLoader(train_set, **cfg.data.train)
     valid_loader = DataLoader(valid_set, **cfg.data.valid)
 
@@ -65,8 +70,8 @@ def main(cfg: DictConfig):
     for epoch in tqdm(range(num_epochs), "Epoch"):
         model.train()
         for x, y in tqdm(train_loader, "Training", leave=False):
-            x = x.to(device)
-            y = y.to(device)
+            x = x.to(device, non_blocking=True)
+            y = y.to(device, non_blocking=True)
 
             y_hat = model(x)
             loss: torch.Tensor = criterion(y_hat, y)
@@ -77,13 +82,11 @@ def main(cfg: DictConfig):
             train_loss.update(loss)
             train_metrics.update(y_hat, y)
 
-            lr_scheduler.step()  # Step on batch
-
         model.eval()
         with torch.no_grad():
             for x, y in tqdm(valid_loader, "Validating", leave=False):
-                x = x.to(device)
-                y = y.to(device)
+                x = x.to(device, non_blocking=True)
+                y = y.to(device, non_blocking=True)
 
                 y_hat = model(x)
                 loss = criterion(y_hat, y)
@@ -91,28 +94,35 @@ def main(cfg: DictConfig):
                 valid_loss.update(loss)
                 valid_metrics.update(y_hat, y)
 
-        # --- Metric handling ---
         writer.add_scalar("lr", lr_scheduler.get_last_lr()[0], epoch)
 
-        valid_loss_epoch = valid_loss.compute()
-        writer.add_scalars("loss", {"train": train_loss.compute(), "valid": valid_loss_epoch})
-
-        for metric_name, metric_value in valid_metrics.compute():
+        writer.add_scalar("loss/train", train_loss.compute(), epoch)
+        for metric_name, metric_value in train_metrics.compute().items():
             writer.add_scalar(metric_name, metric_value, epoch)
 
-        for metric_name, metric_value in train_metrics.compute():
+        valid_loss_epoch = valid_loss.compute()
+        writer.add_scalar("loss/valid", valid_loss_epoch, epoch)
+        for metric_name, metric_value in valid_metrics.compute().items():
             writer.add_scalar(metric_name, metric_value, epoch)
 
         if valid_loss_epoch < best_valid_loss:
-            best_valid_loss = valid_loss_epoch
             torch.save(model.state_dict(), f"{log_dir}/model_state_best_loss.pt")
+            best_valid_loss = valid_loss_epoch
+            epochs_without_improvement = 0
+        else:
+            epochs_without_improvement += 1
+            if epochs_without_improvement == cfg.trainer.patience:
+                logging.info("Maximum epochs without improvement reached. Early stopping.")
+                break
 
         train_loss.reset()
         valid_loss.reset()
         train_metrics.reset()
         valid_metrics.reset()
 
-    logging.info(f"Training ended. Best validation loss: {best_valid_loss}")
+        lr_scheduler.step()
+
+    logging.info(f"Training ended. Best validation loss: {best_valid_loss:.4f}")
     torch.save(model.state_dict(), f"{log_dir}/model_state_last.pt")
     writer.close()
 
